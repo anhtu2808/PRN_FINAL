8b937eb33e21b887a414e92234731d03908801fc
Longnee <longthse184113@fpt.edu.vn>
2025-11-17 17:43:56 +0700

    update axiossetup

--- prn-final/src/Service/AxiosSetup.js
+++ prn-final/src/Service/AxiosSetup.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "https://swd-grading.anhtudev.cloud/api",
   // baseURL: "http://localhost:5064/api",
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5173",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5173/api",
   headers: {
     "Content-Type": "application/json",
   },
